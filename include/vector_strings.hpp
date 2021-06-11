ff32ac47d9cf6573335b0483813a7ab5d2c48577
Martin Jivkov <martin.jivkov5@gmail.com>
2021-06-11 12:58:22 +0300

    fixed books find

--- include/vector_strings.hpp
+++ include/vector_strings.hpp
@@ -8,7 +8,9 @@ public:
         size_t to = 0;
         size_t from = 0;
         bool first = true;
+        int counter = 0;
 		while (to < sentence.length()) {
+            ++counter;
             to = sentence.find(' ', to+1, sentence.length());
             if (to > sentence.length() && first) {
                 words.push_back(sentence);
@@ -26,6 +28,12 @@ public:
             }
             from = to;
             first = false;
+            if (counter == 3) {
+                if ((words[0] == "books" && words[1] == "find" && words[2] == "title") || (words[0] == "books" && words[1] == "find" && words[2] == "author")) {
+                    words.push_back(sentence.substr(from+1,sentence.length()));
+                    break;
+                }
+            }
 		}
     }
 
