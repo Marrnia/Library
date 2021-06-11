6e1e48fb9f235c4a625bd972a97299cdd755adb0
Martin Jivkov <martin.jivkov5@gmail.com>
2021-06-11 12:59:50 +0300

    removed bad functions

--- include/CommandLine.hpp
+++ include/CommandLine.hpp
@@ -7,15 +7,8 @@
 
 class CommandLine {
 private:
-	static library::String text;
-	static bool is_open;
 public:
-	static State open(library::String&);
-	static State save(library::String&);
-	static State saveAs(library::String&);
 	static void help();
-	static State close();
     static void exit();
-	static library::String getText();
-	static void setText(const library::String&);
+
 };
