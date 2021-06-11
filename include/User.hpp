17ecfd5b94ffc0ad11e737dfeac33b05f00e6f11
Martin Jivkov <martin.jivkov5@gmail.com>
2021-06-11 12:55:30 +0300

    Add user

--- include/User.hpp
+++ include/User.hpp
@@ -3,17 +3,43 @@
 #include "String.hpp"
 #include <fstream>
 
+using namespace library;
+
 class User {
 private:
 protected:
-    static library::String username;
-    static library::String password;
-    static bool administrator;
+    String username;
+    String password;
+    bool administrator;
 public:
-    User();
+   /* User();
     static void setUsername(const library::String&);
     static void setPassword(const library::String&);
     static library::String getUsername();
     static library::String getPassword();
-    static bool is_administrator();
+    bool is_administrator();*/
+
+    User();
+    User(const String&);
+    User(const String&, const String&, bool);
+    User(const User&);
+
+    bool operator==(const User&) const;
+    bool operator!=(const User&) const;
+
+    void setUsername(const String&);
+    void setPassword(const String&);
+    void setAdmin(const bool&);
+
+    const String getUsername() const;
+    const String getPassword() const;
+    const bool getAdmin() const;
+    bool is_administrator() const;
+
+    const void save(std::ofstream&) const;
+    void load(std::ifstream&);
+
+    friend std::ostream &operator<<(std::ostream &, const User &);
+
+    void print() const;
 };
--- src/User.cpp
+++ src/User.cpp
@@ -1,30 +1,84 @@
 #include "User.hpp"
 
-library::String User::username;
-library::String User::password;
-bool User::administrator;
 
-User :: User() {
-    username = " ";
-    password = " ";
-    administrator = true;
+User :: User() : username("user"), password("user"), administrator(false) {}
+
+User :: User(const String &username, const String &password, bool admin = false) : username(username), password(password), administrator(admin) {}
+
+User :: User(const User &other) {
+    this->username = other.username;
+    this->password = other.password;
+    this->administrator = other.administrator;
+}
+
+bool User :: operator==(const User &other) const {
+    if (this->username == other.username && this->password == other.password) {
+        return true;
+    }
+    return false;
+}
+
+bool User :: operator!=(const User &other) const {
+    return (!(*this == other));
 }
 
 void User :: setUsername(const library::String& name) {
-    username = name;
+    this->username = name;
 }
 
-void User :: setPassword(const library::String& pass) {
-    password = pass;
+void User :: setPassword(const String& pass) {
+    this->password = pass;
 }
-library::String User :: getUsername() {
+
+void User :: setAdmin(const bool& admin) {
+    this->administrator = admin;
+}
+
+const String User :: getUsername() const{
     return username;
 }
 
-library::String User :: getPassword() {
+const String User :: getPassword() const{
     return password;
 }
 
-bool User :: is_administrator() {
+const bool User :: getAdmin() const {
     return administrator;
+}
+
+bool User :: is_administrator() const {
+    return administrator;
+}
+
+const void User::save(std::ofstream &file) const
+{
+    file << this->username << "\n";
+    file << this->password << "\n";
+    file << std::boolalpha << this->administrator;
+}
+
+void User::load(std::ifstream& file) {
+    char *buffer = new char[1000];
+    file.getline(buffer, '\n');
+    this->username = buffer;
+    file.clear();
+    file.ignore(10000, '\n');
+    file.getline(buffer, '\n');
+    this->password = buffer;
+    file.clear();
+    file.ignore(10000,'\n');
+    file >> this->administrator;
+    delete [] buffer;
+    // file >> this->username >> this->password >> this->admin;
+    // file.ignore(10000, '\n');
+}
+
+std::ostream &operator<<(std::ostream &out, const User &user)
+{
+    out << "Username: " << user.getUsername() << '\n';
+    out << "Password: " << user.getPassword() << '\n';
+    out << "Admin: ";
+    (user.is_administrator() ? (out << "Yes") : (out << "No"));
+    out << '\n';
+    return out;
 }
