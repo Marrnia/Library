63792f0182c8af0bcf2863ce34943e462e85b986
Martin Jivkov <martin.jivkov5@gmail.com>
2021-06-11 12:55:57 +0300

    Added users and books

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include "String.hpp"
 #include "CommandLine.hpp"
 #include "State.hpp"
-#include "LibraryCommands.hpp"
-#include "Administrator.hpp"
 #include "Vector.hpp"
 #include "vector_strings.hpp"
+#include "UserDatabase.hpp"
+#include "BookDatabase.hpp"
+#include "Database.hpp"
 
 using namespace library;
 
@@ -15,6 +17,15 @@ private:
     library::String command;
     library::String file_name;
     bool logged;
+    bool administrator;
+
+    String targetFile;
+
+    std::ifstream file_open;
+    std::ofstream file_save;
+
+    UserDatabase users;
+    BookDatabase books;
 
     void choice();
  
